79fb26fa0c30999c7de2717dd28bc70bd5ddb455
prichardsondev <prichardsondev@gmail.com>
2025-04-21 06:42:28 -0400

    🚀 Secure deploy with external config and refresh hook

--- start.sh
+++ start.sh
@@ -1,2 +1,7 @@
-cd /home/rat/pi-api
-pm2 start ./start.sh --name vet-api
+#!/bin/bash
+
+# Step 1: Run the credential refresher from secure location
+/home/rat/.aws-bootstrap/refresh-creds.sh
+
+# Step 2: Start your Node.js app (assumes it's in the same folder)
+node server.js
